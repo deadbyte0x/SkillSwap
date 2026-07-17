362c752a13603aab7efb6272160fbeb76ead63ec
Masstran <masstran@yandex.ru>
2026-07-17 09:52:22 +0300

    feature/make-bad-pages-a-little-better

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,10 +1,15 @@
 // TODO: реализовать страницу ProfilePage
 
+import { Header } from '@/widgets/Header'
+
 export default function ProfilePage() {
   return (
-    <main>
-      <h1>ProfilePage</h1>
-      <p>Страница в разработке</p>
-    </main>
+    <>
+      <Header />
+      <main style={{textAlign: "center"}}>
+        <h1>ProfilePage</h1>
+        <p>Страница в разработке</p>
+      </main>
+    </>
   )
 }
--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,10 +1,15 @@
 // TODO: реализовать страницу RegisterPage
 
+import { Header } from '@/widgets/Header'
+
 export default function RegisterPage() {
   return (
-    <main>
-      <h1>RegisterPage</h1>
-      <p>Страница в разработке</p>
-    </main>
+    <>
+      <Header variant={'pure'} />
+      <main style={{textAlign: "center"}}>
+        <h1>RegisterPage</h1>
+        <p>Страница в разработке</p>
+      </main>
+    </>
   )
 }
